3137d8b8625b149d2dfd45d8c71f3dc54b6ece9b
k.shabordin <(null)>
2014-06-18 18:32:45 +0400

    PEP8 reformatting

--- python/main.py
+++ python/main.py
@@ -1,10 +1,10 @@
-from objects import *
+import miniengine
+import objects
 import shaders
 
-e = Engine(800, 600)
+e = miniengine.Engine(800, 600)
 
-e.add_object(createPlane(2,2).set_material(shaders.zebro()))
-e.add_object(createTriangle(1).set_material(shaders.simple()))
+e.add_object(objects.plane(2, 2).set_material(shaders.zebro()))
+e.add_object(objects.triangle(1).set_material(shaders.simple()))
 
 e.loop()
-
--- python/objects.py
+++ python/objects.py
@@ -1,28 +1,27 @@
 from miniengine import *
 from math import *
 
-def createPlane(width,height):
-    w = (float)(width)/2.0
-    h = (float)(height)/2.0
+
+def plane(width, height):
+    w = float(width) / 2.0
+    h = float(height) / 2.0
     return Mesh(
-    [
-        -w, h, 0.0,  0.0, 0.0,
-        -w,-h, 0.0,  0.0, 1.0,
-         w,-h, 0.0,  1.0, 1.0,
-         w, h, 0.0,  1.0, 0.0
-    ],
-    [0,1,3,2,3,1])
+        [
+            -w, h, 0.0, 0.0, 0.0,
+            -w, -h, 0.0, 0.0, 1.0,
+            w, -h, 0.0, 1.0, 1.0,
+            w, h, 0.0, 1.0, 0.0
+        ],
+        [0, 1, 3, 2, 3, 1])
 
-def createTriangle(radius):
-    y = (float)(radius) * -sin(degrees(30))
-    x = (float)(radius) * cos(degrees(30))
+
+def triangle(radius):
+    y = float(radius) * -sin(degrees(30))
+    x = float(radius) * cos(degrees(30))
     return Mesh(
-    [
-        -x,-y, 0.0,      0.0, 0.0,
-         x,-y, 0.0,      0.0, 1.0,
-        .0, radius,0.0,  0.5, 1.0
-    ],
-    [0,1,2])
-
-
-
+        [
+            -x, -y, 0.0, 0.0, 0.0,
+            x, -y, 0.0, 0.0, 1.0,
+            .0, radius, 0.0, 0.5, 1.0
+        ],
+        [0, 1, 2])
