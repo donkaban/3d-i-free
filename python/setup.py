7edaabe1a0c2d6a8b872cbbeb61d6c7294578cb1
k.shabordin <k.shabordin@gmail.com>
2014-06-27 17:19:45 +0400

    add deploy script

---
+++ python/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+APP = ['main.py']
+OPTIONS = {'argv_emulation': True, 'includes': ['EXTERNAL LIBRARY'],}
+
+setup(
+    app=APP,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+    )
---
+++ python_deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ./python
+python ./setup.py py2app
+rm -rf ./build
+cp -R ./data ./dist/main.app/Contents/Resources/
