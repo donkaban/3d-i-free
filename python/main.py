8b6fb3da01997d8439c9d1248f5aa4b0569c508e
k.shabordin <k.shabordin@gmail.com>
2014-06-29 00:59:16 +0400

    move common data folder/ fix deploy script

--- python/main.py
+++ python/main.py
@@ -2,9 +2,9 @@ from miniGL import *
 
 e = Engine(800, 600)
 
-t1 = Texture('data/synqera.png')
-t2 = Texture('data/ifree.png')
-m1 = Material('SIMPLE', 'data/shaders/base.vsh', 'data/shaders/simple.fsh')
+t1 = Texture('../data/synqera.png')
+t2 = Texture('../data/ifree.png')
+m1 = Material('SIMPLE', '../data/shaders/base.vsh', '../data/shaders/simple.fsh')
 
 e.add_object(geometry.plane(40, 40).set_material(m1).translate(0, 0, -60))
 
