85e0b02dd70b45b57f661101513a6b8672dffa4f
k.shabordin <k.shabordin@gmail.com>
2014-09-18 17:36:23 +0400

    WiPP

--- countlines.sh
+++ countlines.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-echo 'PYTHON :'
 wc -l `find ./python -name '*.py' `
-
-echo 'C++ :'
-wc -l `find ./mini_c++ -name '*.cpp' -o -name '*.h' `
 
--- python/main.py
+++ python/main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from miniGL import *
 
 e = Engine(800, 600)
